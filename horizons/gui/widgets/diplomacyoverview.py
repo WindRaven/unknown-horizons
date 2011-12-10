e59324770692d8a6f8ea5638f77242dbbcd7dbb8
stefan <stefan@stefan-K53SV.(none)>
2011-12-10 17:31:55 +0200

    Made widget for changing diplomacy

--- horizons/gui/widgets/diplomacyoverview.py
+++ horizons/gui/widgets/diplomacyoverview.py
@@ -23,6 +23,10 @@ from fife.extensions import pychan
 from horizons.util.gui import load_uh_widget
 from horizons.util import Callback
 from horizons.util.changelistener import metaChangeListenerDecorator
+from horizons.gui.widgets.diplomacywidget import DiplomacyWidget
+
+from horizons.gui.widgets.unitoverview import StanceWidget
+
 import math
 
 class DiplomacyOverview(object):
@@ -35,43 +39,101 @@ class DiplomacyOverview(object):
 	def __init__(self, session):
 		self.session = session
 		
+		# Players need to be inited later
+		self._inited = False
+		
+		# True if and only if the widget is currently in the process of being hidden	
+		self._hiding_widget = False 
+		self._gui = None
+		
+	def init(self):
+		self.players = set(self.session.world.players)
+		self.players.add(self.session.world.pirate)
+		self.players.discard(self.session.world.player)
+		self.players.discard(None) # e.g. when the pirate is disabled
+		
+		self.player = self.session.world.player
+		self._inited = True
+
 		self._init_gui()
-		self._hiding_widget = False # True if and only if the widget is currently in the process of being hidden
-	
+		
 	def show(self):
 		# don't show if there are no messages
 		#if len(self._messages) == 0:
 		#	return
+		if not self._inited:
+			self.init()
 		
-		players = set(self.session.world.players)
-		players.add(self.session.world.pirate)
-		players.discard(self.session.world.player)
-		players.discard(None) # e.g. when the pirate is disabled
-		player = self.session.world.player
 		self._gui.show()
 		self.session.ingame_gui.on_switch_main_widget(self)
 		
-		angle_incr = 2 * math.pi / len(players)
-		angle = 0
-		r = 140
-		icon_path='content/gui/images/tabwidget/emblems/emblem_%s.png'
+	def show_diplomacy_widget(self, player):
+		container_diplo_widget = self._gui.findChild(name = "diplo_widget")
+		container_diplo_widget.removeAllChildren()
+		container_diplo_widget.addChild(self.diplomacy_widgets[player])
+		
+
+	def hide(self):
+		if not self._hiding_widget:
+			self._hiding_widget = True
+			self.session.ingame_gui.on_switch_main_widget(None)
+			self._gui.hide()
+			self._hiding_widget = False
+			self.session.speed_unpause(True)
+
+	def is_visible(self):
+		return self._gui.isVisible() if self._gui else False
+
+	def toggle_visibility(self):
+		if self.is_visible():
+			self.hide()
+		else:
+			self.show()
+
+	def _init_gui(self):
+		"""Initial init of gui."""
+		
+		self._gui = load_uh_widget("diplo_log.xml")
+		self._gui.mapEvents({
+		  'cancelButton' : self.hide
+		  })
 		
 		container_left = self._gui.findChild(name = "left_overview")
 		container_right = self._gui.findChild(name = "right_overview")
+		
+		
+		#diplomacy_widget = DiplomacyWidget()
+		#diplomacy_widget.init(list(self.players)[0])
+		#container_left.addChild(diplomacy_widget)
+		
+		
+		
+		self.diplomacy_widgets  = {}
+		for player in self.players:
+			diplomacy_widget = DiplomacyWidget()
+			diplomacy_widget.init(player)
+			self.diplomacy_widgets[player] = diplomacy_widget 
+			
+		self.show_diplomacy_widget(list(self.players)[1])
+		
+		angle_incr = 2 * math.pi / len(self.players)
+		angle = 0
+		r = 140
+		icon_path='content/gui/images/tabwidget/emblems/emblem_%s.png'
 		
 		#x_offset and y_offset are the centers of each circles	
 		x_offset_left = round(container_left.height / 3.5)
 		y_offset_left = round(container_left.width / 1.7)
 	
-		icon_center_left = pychan.Icon(image = icon_path % player.color.name,
-		 position = (int(x_offset_left), int(y_offset_left)))
-		container_left.addChild(icon_center_left)
+		#icon_center_left = pychan.Icon(image = icon_path % self.player.color.name,
+		#position = (int(x_offset_left), int(y_offset_left)))
+		#container_left.addChild(icon_center_left)
 		
 		x_offset_right = round(container_right.height / 3.5)
 		y_offset_right = round(container_right.width / 1.7)
 		
 		#draws other player buttons on both sides
-		for player in players:
+		for player in self.players:
 			
 			x = round ( math.cos(angle) * r )
 			y = round( math.sin(angle) * r )	
@@ -84,32 +146,7 @@ class DiplomacyOverview(object):
 			container_right.addChild(icon_right)
 	
 			angle += angle_incr
-			
-
-	def hide(self):
-		if not self._hiding_widget:
-			self._hiding_widget = True
-			self.session.ingame_gui.on_switch_main_widget(None)
-			self._gui.hide()
-			self._hiding_widget = False
-			self.session.speed_unpause(True)
-
-	def is_visible(self):
-		return self._gui.isVisible()
-
-	def toggle_visibility(self):
-		if self.is_visible():
-			self.hide()
-		else:
-			self.show()
-
-	def _init_gui(self):
-		"""Initial init of gui."""
-		self._gui = load_uh_widget("diplo_log.xml")
-		self._gui.mapEvents({
-		  'cancelButton' : self.hide
-		  })
-
+	
 	def _redraw(self):
 		"""Redraws gui. Necessary when current  has changed."""
 		pass
--- horizons/gui/widgets/diplomacywidget.py
+++ horizons/gui/widgets/diplomacywidget.py
@@ -1,32 +1,66 @@
+from fife.extensions import pychan
+from horizons.util.gui import load_uh_widget, get_res_icon
+from horizons.util import Callback
+from horizons.gui.widgets import TooltipIcon
+from horizons.gui.tabs.tabinterface import TabInterface
+
+from horizons.command.diplomacy import AddAllyPair, AddNeutralPair, AddEnemyPair
+
 class DiplomacyWidget(pychan.widgets.Container):
 	"""TODO"""
-	def __init__(self, **kwargs):
-		super(StanceWidget, self).__init__(size=(245,50), **kwargs)
+	def __init__(self):
+		super(DiplomacyWidget, self).__init__(size=(245,50),position=(0,0))
 		widget = load_uh_widget('diplomacywidget.xml')
 		self.addChild(widget)
-
-	def init(self, instance):
-		self.instance = instance
-		self.toggle_stance()
-		self.mapEvents({
-			'aggressive': Callback(self.set_stance, 'aggressive'),
-			'hold_ground': Callback(self.set_stance, 'hold_ground'),
-			'none': Callback(self.set_stance, 'none'),
-			'flee': Callback(self.set_stance, 'flee')
-			})
-
+		self.widget = widget
+		
+	def init(self, player):
+		self.widget.mapEvents({
+			'ally' : self.add_friend,
+			'neutral' : self.add_neutral,
+			'enemy' : self.add_enemy})
+		
+		self.local_player = player.session.world.player
+		self.player = player
+		self.diplomacy = player.session.world.diplomacy
+		self.toggle_diplomacy_state()
+	
 	def remove(self, caller=None):
 		"""Removes instance ref"""
 		self.mapEvents({})
 		self.instance = None
+		
+	def toggle_diplomacy_state(self):
+		if self.diplomacy.are_friends(self.local_player, self.player):
+			state = 'ally'
+		elif self.diplomacy.are_neutral(self.local_player, self.player):
+			state = 'neutral'
+		else:
+			state = 'enemy'
+			
+		self.findChild(name='enemy').set_inactive()
+		self.findChild(name='neutral').set_inactive()
+		self.findChild(name='ally').set_inactive()
+		self.findChild(name=state).set_active()
+		
+	def add_friend(self):
+		"""
+		Callback for setting ally status between local player and tab's player
+		"""
+		AddAllyPair(self.player, self.local_player).execute(self.player.session)
+		self.toggle_diplomacy_state()	
 
-	def set_stance(self, stance):
-		self.instance.set_stance(stance)
-		self.toggle_stance()
+	def add_neutral(self):
+		"""
+		Callback for setting neutral status between local player and tab's player
+		"""
+		AddNeutralPair(self.player, self.local_player).execute(self.player.session)	
+		self.toggle_diplomacy_state()
 
-	def toggle_stance(self):
-		self.findChild(name='aggressive').set_inactive()
-		self.findChild(name='hold_ground').set_inactive()
-		self.findChild(name='none').set_inactive()
-		self.findChild(name='flee').set_inactive()
-		self.findChild(name=self.instance.stance).set_active()
+	def add_enemy(self):
+		"""
+		Callback for setting enemy status between local player and tab's player
+		"""
+		AddEnemyPair(self.player, self.local_player).execute(self.player.session)
+		self.toggle_diplomacy_state()
+
